1e5547eb7954c7f8a38265544cd567ac8666f76f
Arturo Gonzaga <apgonzagas@gmail.com>
2025-04-21 10:10:31 -0500

    Update 03_data_range_exploration.sql

--- scripts/03_data_range_exploration.sql
+++ scripts/03_data_range_exploration.sql
@@ -1,1 +1,33 @@
+/*
+===============================================================================
+Date Range Exploration 
+===============================================================================
+Purpose:
+    - To determine the temporal boundaries of key data points.
+    - To understand the range of historical data.
 
+SQL Functions Used:
+    - MIN(), MAX(), DATEDIFF()
+===============================================================================
+*/
+
+-- ====================================================================== 
+-- 3) Date Exploration
+-- ======================================================================
+
+-- Find the date of the first and last order
+-- How many years of sales are avaiable
+
+SELECT
+MIN(order_date) AS first_order_date,
+MAX(order_date) AS last_order_date,
+DATEDIFF(MAX(order_date), MIN(order_date))/365 AS order_range_years
+FROM gold.fact_sales;
+
+-- Find the youngest and the oldest customer
+SELECT 
+MIN(birthdate) AS oldest_birthdate,
+DATEDIFF(NOW(), MIN(birthdate))/365 AS oldest_age,
+MAX(birthdate) AS youngest_birhdate,
+DATEDIFF(NOW(), MAX(birthdate))/365 AS youngest_age
+FROM gold.dim_customers;
