1614b3bb939606f028fef53aa2a0cff1cde08cd2
Arturo Gonzaga <apgonzagas@gmail.com>
2025-04-21 10:17:35 -0500

    Update 10_data_segmentation.sql

--- scripts/10_data_segmentation.sql
+++ scripts/10_data_segmentation.sql
@@ -1,1 +1,37 @@
+/*
+===============================================================================
+Data Segmentation Analysis
+===============================================================================
+Purpose:
+    - To group data into meaningful categories for targeted insights.
+    - For customer segmentation, product categorization, or regional analysis.
+
+SQL Functions Used:
+    - CASE: Defines custom segmentation logic.
+    - GROUP BY: Groups data into segments.
+===============================================================================
+*/
+
+-- ====================================================================== 
+-- 10) Part-to-Whole (Proportional)
+-- ======================================================================
+
+-- Which categories contribute the most to overall sales?
+
+WITH category_sales AS (
+SELECT 
+	category,
+    SUM(sales_amount) AS total_sales
+FROM datawarehouseanalytics.fact_sales AS f
+LEFT JOIN datawarehouseanalytics.dim_products AS p
+ON p.product_key = f.product_key
+GROUP BY category
+)
+SELECT 
+	category,
+    total_sales,
+    SUM(total_sales) OVER() AS overall_sales,
+    CONCAT(ROUND((total_sales / SUM(total_sales) OVER()) *100, 2), '%') AS percentage_of_total
+FROM category_sales
+ORDER BY total_sales DESC; 
 
