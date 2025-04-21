3e70831d35d727e4cc27ef28405a7ee2af89eb98
Arturo Gonzaga <apgonzagas@gmail.com>
2025-04-21 10:13:33 -0500

    Update 04_measures_exploration.sql

--- scripts/04_measures_exploration.sql
+++ scripts/04_measures_exploration.sql
@@ -15,6 +15,10 @@ SQL Functions Used:
 -- 4) Measures Exploration
 -- ======================================================================
 
+-- ====================================================================== 
+-- 4) Measures Exploration
+-- ======================================================================
+
 -- Find the Total Sales
 SELECT SUM(sales_amount) AS total_sales FROM datawarehouseanalytics.fact_sales;
 
@@ -30,7 +34,7 @@ SELECT COUNT(DISTINCT order_number) AS total_orders FROM datawarehouseanalytics.
 
 -- Find the Total number of products
 SELECT COUNT(product_name) AS total_products FROM datawarehouseanalytics.dim_products;
-SELECT COUNT(DISTINCT product_name) AS total_products FROM datawarehouseanalytics.dim_products;
+SELECT COUNT(DISTINCT product_name) AS total_products FROm datawarehouseanalytics.dim_products;
 
 -- Find the Total number of customers
 SELECT COUNT(customer_key) AS total_customers FROM datawarehouseanalytics.dim_customers;
@@ -38,3 +42,16 @@ SELECT COUNT(customer_key) AS total_customers FROM datawarehouseanalytics.dim_cu
 -- Find the total number of customers that has placed an order
 SELECT COUNT(DISTINCT customer_key) AS total_customers FROM datawarehouseanalytics.fact_sales;
 
+-- Generate a Report that shows all key metrics of the business
+
+SELECT 'Total Sales' AS measure_name, SUM(sales_amount) AS measure_value FROM datawarehouseanalytics.fact_sales
+UNION ALL
+SELECT 'Total Quantity' AS measure_name, SUM(quantity) AS measure_value FROM datawarehouseanalytics.fact_sales
+UNION ALL
+SELECT 'Average Price', AVG(price) AS avg_price FROM datawarehouseanalytics.fact_sales
+UNION ALL
+SELECT 'Total Nr. Orders', COUNT(DISTINCT order_number) AS total_orders FROM datawarehouseanalytics.fact_sales
+UNION ALL
+SELECT 'Total Nr. Products', COUNT(product_name) AS total_products FROM datawarehouseanalytics.dim_products
+UNION ALL
+SELECT 'Total Nr. Customers', COUNT(customer_key) AS total_customers FROM datawarehouseanalytics.fact_sales;
