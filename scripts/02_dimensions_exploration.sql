0b58f082932811e68dd18d3d733462025cc33160
Arturo Gonzaga <apgonzagas@gmail.com>
2025-04-21 10:09:12 -0500

    Update 02_dimensions_exploration.sql

--- scripts/02_dimensions_exploration.sql
+++ scripts/02_dimensions_exploration.sql
@@ -1,1 +1,27 @@
+/*
+===============================================================================
+Dimensions Exploration
+===============================================================================
+Purpose:
+    - To explore the structure of dimension tables.
+	
+SQL Functions Used:
+    - DISTINCT
+    - ORDER BY
+===============================================================================
+*/
 
+-- ====================================================================== 
+-- 2) Dimensions Exploration 
+-- ======================================================================
+
+-- Explore All Countries our customers come from.
+SELECT DISTINCT 
+	country 
+FROM datawarehouseanalytics.dim_customers
+ORDER BY country;
+
+-- Explore All Categories "The major Divisions"
+SELECT DISTINCT category, subcategory, product_name 
+FROM gold.dim_products 
+ORDER BY category, subcategory, product_name;
