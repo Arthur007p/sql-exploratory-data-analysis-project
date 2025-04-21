141a1a7a6e3f5b9d8be62157042f9c078e8f143c
Arturo Gonzaga <apgonzagas@gmail.com>
2025-04-21 10:16:04 -0500

    Update 08_cumulative_analysis.sql

--- scripts/08_cumulative_analysis.sql
+++ scripts/08_cumulative_analysis.sql
@@ -1,1 +1,37 @@
+/*
+===============================================================================
+Cumulative Analysis
+===============================================================================
+Purpose:
+    - To calculate running totals or moving averages for key metrics.
+    - To track performance over time cumulatively.
+    - Useful for growth analysis or identifying long-term trends.
+
+SQL Functions Used:
+    - Window Functions: SUM() OVER(), AVG() OVER()
+===============================================================================
+*/
+
+-- ====================================================================== 
+-- 8) Cumulative Analysis
+-- ======================================================================
+
+-- Calculate the total sales per month
+-- and the running total of sales over time
+
+SELECT
+	order_date,
+    total_sales,
+    SUM(total_sales) OVER(PARTITION BY order_date ORDER BY order_date) AS running_total_sales,
+    AVG(avg_price) OVER(PARTITION BY order_date ORDER BY order_date) AS moving_average_price
+FROM
+(
+SELECT
+	DATE_FORMAT(order_date, '%Y') AS order_date,
+    SUM(sales_amount) AS total_sales,
+    AVG(price) AS avg_price
+FROM datawarehouseanalytics.fact_sales
+WHERE order_date IS NOT NULL AND order_date != 0
+GROUP BY DATE_FORMAT(order_date, '%Y')
+) AS t;
 
